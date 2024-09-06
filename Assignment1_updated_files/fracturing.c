1b28df843bafbd99162d20fd5c4425b6ac7c1529
gesicab <ge679022@ucf.edu>
2024-09-06 20:57:59 +0000

    added distance and perimeter

--- Assignment1_updated_files/fracturing.c
+++ Assignment1_updated_files/fracturing.c
@@ -4,12 +4,14 @@
 
 double calculateDistance() {
 
+    double x1, y1, x2, y2, distance;    // declares the variables for coordinates
+
     // asks the user for the first coordinates
     printf("Enter coordinates for point 1 (x1,y1): ");
     scanf("%lf %lf", &x1, &y2); //saves the data into the variables
 
     // asks the user for the first coordinates 
-    printf("Enter coordinates for point 2");
+    printf("Enter coordinates for point 2: ");
     scanf("%lf %lf", &x1, &y2); //saves the data into the variables
 
     // calculates distance
@@ -24,10 +26,21 @@ double calculateDistance() {
     return distance;
 }
 
+double calculatePerimeter() {
+    
+    double distance, perimeter, difficulty;
+    distance = calculateDistance();  // calculates distance by calling the distance function
+    perimeter = 3.14159 * distance;  // calculates perimeter
+    printf("The perimeter of the city is %.2f\n", perimeter);
+    printf("On a scale 1.0 to 5.0, how difficult was this function for you? \n");
+    scanf("%lf", &difficulty);
+
+    return difficulty;
+}
 
 int main(int argc, char**aargv) {
     calculateDistance();
-    
+    calculatePerimeter();
     return 0;
 }
  
