0a1e56907fd79798bd1332fe7c1d6f1ebdff5b01
gesicab <ge679022@ucf.edu>
2024-09-09 02:42:52 +0000

    small edits

--- Assignment1_updated_files/fracturing.c
+++ Assignment1_updated_files/fracturing.c
@@ -1,21 +1,24 @@
+// Gesica Borecki
+// UCFID: 5487663
+
 #include <stdio.h>
 #include <math.h>   // for math calculations
 #define PI 3.14159  //defines the constant for pi
 
-// helper function to avoid repeation in print statements (idea from chatGPT)
+// helper function to calculate distance seperate from printing it (idea from chatGPT)
 double calculateusedDistance(double x1, double y1, double x2, double y2) {
     return sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
 }
 
 // function to get coordinates
 double calculateDistance() {
-    double x1, y1, x2, y2, distance;    // declares the variables for coordinates
+    double x1, y1, x2, y2, distance;    // initializes the variables for coordinates
 
     // asks the user for coordinates
     printf("Enter coordinates for point 1 (x1 y1): \n");
-    scanf("%lf %lf", &x1, &y1);     // saves the data into the variables
+    scanf("%lf %lf", &x1, &y1);
     printf("Enter coordinates for point 2 (x2 y2): \n");
-    scanf("%lf %lf", &x2, &y2);     // saves the data into the variables
+    scanf("%lf %lf", &x2, &y2);
 
     // calculates distance by calling the helper function
     distance = calculateusedDistance(x1, y1, x2, y2);   // (idea from chatGPT)
@@ -29,37 +32,34 @@ double calculateDistance() {
     return distance;
 }
 
-// function to calculate perimeter and difficulty
+// function to calculate perimeter
 double calculatePerimeter() {
-    double x1, y1, x2, y2;
-    double distance = calculateusedDistance(x1, y1, x2, y2); // finds the diameter through the distance function
-    double radius = distance / 2; // calculates the radius
-    double perimeter = 2 * PI * radius  ;  // calculates perimeter
-    double difficulty;  // For difficulty rating (idea from ChatGPT)
+    double x1, y1, x2, y2;  // initializing variables
+
 
     // asks the user for coordinates
     printf("Enter coordinates for point 1 (x1 y1): \n");
-    scanf("%lf %lf", &x1, &y1);     // saves the data into the variables
+    scanf("%lf %lf", &x1, &y1);
     printf("Enter coordinates for point 2 (x2 y2): \n");
-    scanf("%lf %lf", &x2, &y2);     // saves the data into the variables
+    scanf("%lf %lf", &x2, &y2);
+
+    // calculates distance (diameter)
+    double distance = calculateusedDistance(x1, y1, x2, y2); // finds the diameter through the distance function
+    double radius = distance / 2; // calculates the radius
+    double perimeter = 2 * PI * radius  ;  // calculates perimeter
 
     // displays the output for perimeter and asks for difficulty
     printf("Point #1 entered: x1 = %.2f; y1 = %.2f\n", x1, y1);
     printf("Point #2 entered: x2 = %.2f; y2 = %.2f\n", x2, y2);
     printf("The perimeter of the city is %.2f\n", perimeter);
-    printf("On a scale 1.0 to 5.0, how difficult was this function for you? \n");
-    scanf("%lf", &difficulty);
 
-    return difficulty;
+    // returns difficulty level
+    return 4.0;
 }
 
-// function to calculate area and difficulty
+// function to calculate area
 double calculateArea() {
-    double x1, y1, x2, y2;
-    double distance = calculateusedDistance(x1, y1, x2, y2); // finds the diameter (distance)
-    double radius = distance / 2; // calculates the radius
-    double area = PI * pow(radius, 2); // calls the variable PI to calculate area
-    double difficulty;
+    double x1, y1, x2, y2;  // initializing variables
 
     // asks the user for coordinates
     printf("Enter coordinates for point 1 (x1 y1): \n");
@@ -67,17 +67,21 @@ double calculateArea() {
     printf("Enter coordinates for point 2 (x2 y2): \n");
     scanf("%lf %lf", &x2, &y2);     // saves the data into the variables
 
+    // calculates area
+    double distance = calculateusedDistance(x1, y1, x2, y2); // finds the diameter (distance)
+    double radius = distance / 2; // calculates the radius
+    double area = PI * pow(radius, 2); // calls the variable PI to calculate area
+
     // displays the output for area and asks for difficulty
     printf("Point #1 entered: x1 = %.2f; y1 = %.2f\n", x1, y1);
     printf("Point #2 entered: x2 = %.2f; y2 = %.2f\n", x2, y2);
     printf("The area of the city is %.2f\n", area);
-    printf("On a scale 1.0 to 5.0, how difficult was this function for you? \n");
-    scanf("%lf", &difficulty);
 
-    return difficulty;
+    // difficulty level
+    return 4.0;
 }
 
-// function to calculate width and difficulty
+// function to calculate width
 double calculateWidth() {
     double x1, y1, x2, y2;
 
@@ -94,15 +98,11 @@ double calculateWidth() {
     printf("Point #2 entered: x2 = %.2f; y2 = %.2f\n", x2, y2);
     printf("The width of the city encompassed by your request is %.2f\n", width);
 
-    // asks for difficulty
-    double difficulty;
-    printf("On a scale 1.0 to 5.0, how difficult was this function for you? \n");
-    scanf("%lf", &difficulty);
-
-    return difficulty;
+    // difficulty level
+    return 2.0;    // difficulty level
 }
 
-// function to calculate height and difficulty
+// function to calculate height
 double calculateHeight() {
     double x1, y1, x2, y2;
     double height = fabs(y2 - y1);   // calculating height using absolute value
@@ -118,12 +118,8 @@ double calculateHeight() {
     printf("Point #2 entered: x2 = %.2f; y2 = %.2f\n", x2, y2);
     printf("The height of the city encompassed by your request is %.2f\n", height);
 
-    // asks for difficulty
-    double difficulty;
-    printf("On a scale 1.0 to 5.0, how difficult was this function for you? \n");
-    scanf("%lf", &difficulty);
-
-    return difficulty;
+    // difficulty level
+    return 2.0;
 }
 
 // calls the functions so they can be executed
